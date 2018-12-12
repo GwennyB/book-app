9e93c2b1897fcb79836e7aafffa01ab5dac90065
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-12-11 22:42:07 -0800

    searches now render, but not tolerant to missing fields; not styled

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 'use strict';
 
+// add dependencies
 const express = require('express');
 const app = express();
-
-const ejs = require('ejs');
 const superagent = require('superagent');
-
-
-// require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+// middleware (captures req/res and modifies)
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./public'));
+// app.use(express.static('./public')); // for serving static content
 
+require('dotenv').config();
 
 //set templating engine
 app.set('view engine', 'ejs');
@@ -42,36 +40,43 @@ function getBooks(request,response) {
 
 function Book (data) {
   this.title = data.volumeInfo.title || 'Title not listed.';
-  this.image = data.volumeInfo.imageLinks.smallThumbnail || 'http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg';
+  this.image = data.volumeInfo.imageLinks.thumbnail || 'http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg'; //
   this.authors = data.volumeInfo.authors || 'Authors not listed.';
   this.summary = data.volumeInfo.description || 'Summary not available.'
 }
 
-Book.fetch = function (request,response) {
+Book.fetch = function (handler,response) {
   // request data from API
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${request.queryType}:${request.query}`
-
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${handler.queryType}:${handler.query}`
   superagent.get(url)
     .then(results => {
-      Book.makeBooks(results.body.items,response);})
-    .catch(error => handleError(error));
+      let arrOfBooks = Book.makeBooks(results.body.items);
+      return arrOfBooks
+    })
+    .then (results => {
+      response.render('./pages/searches/show', { allBooks: results})
+    })
 }
 
-Book.makeBooks = function (bookData,response) {
+Book.makeBooks = function (bookData) {
   // build array to return to render
-  const allBooks = [];
   // make new Book objects for each item in incoming bookData
-  bookData.slice(0,10).map( entry => {
-    allBooks.push(new Book(entry));
-  })
-  response.render('./pages/searches/show', allBooks);
+  let allBooks = [];
+  if (bookData.length < 1) {
+    return allBooks;
+  } else {
+    if (bookData.length > 10) {
+      bookData = bookData.slice[0,10];
+    }
+    allBooks = bookData.map( entry => {
+      console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~book: ${entry.volumeInfo.title} ~~~` ,entry.volumeInfo.imageLinks);
+      let book = new Book(entry);
+      return book;
+    })
+    // console.log('allBooks @ 74: ',allBooks.length);
+    return allBooks;
+  }
 }
-
-function handleError(error,status) {
-  console.error('Sorry, there was an error.');
-}
-
-
 
 app.listen(PORT, () => {
   console.log(`listening to port: ${PORT}`);
